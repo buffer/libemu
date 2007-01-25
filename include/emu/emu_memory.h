b2cf042e955ae6c3ad2ff23703209e342c108708
dp <dp@carnivore.it>
2007-01-25 21:12:31 +0000

    memory interface  - s/word/dword/  - _read() by reference, return value is for error reporting

--- include/emu/emu_memory.h
+++ include/emu/emu_memory.h
@@ -9,8 +9,8 @@ struct emu_memory;
 struct emu_memory *emu_memory_new();
 void emu_memory_free(struct emu_memory *em);
 
-uint32_t emu_memory_read_dword(struct emu *e, struct emu_memory *em, void *p);
-void emu_memory_write_dword(struct emu *e, struct emu_memory *em, void *p, uint32_t word);
+uint32_t emu_memory_read_dword(struct emu *e, struct emu_memory *em, void *p, uint32_t *word);
+void emu_memory_write_dword(struct emu *e, struct emu_memory *em, void *p, uint32_t dword);
 
 
 #endif // HAVE_EMU_MEMORY_H
