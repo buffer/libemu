3723f56c7d9f2c28a6bbf672e4893db25bd47ba7
common <common@carnivore.it>
2007-02-13 01:21:52 +0000

    libemu  - add broken definitions for adc and cmp or sbb sub and xor, instruction format as well as instruction copied from add, as the 8 different commands feature 6, apart from the operation and flags, equal fn's, this might be macro'd later  - the itables got extended too, and here i'd expect them to be correct, even though the param list was copied from add

--- include/emu/emu_cpu_functions.h
+++ include/emu/emu_cpu_functions.h
@@ -43,4 +43,64 @@ int32_t instr_add_03(struct emu_cpu *c, struct instruction *i);
 int32_t instr_add_04(struct emu_cpu *c, struct instruction *i);
 int32_t instr_add_05(struct emu_cpu *c, struct instruction *i);
 
+/* or */
+int32_t instr_or_08(struct emu_cpu *c, struct instruction *i);
+int32_t instr_or_09(struct emu_cpu *c, struct instruction *i);
+int32_t instr_or_0a(struct emu_cpu *c, struct instruction *i);
+int32_t instr_or_0b(struct emu_cpu *c, struct instruction *i);
+int32_t instr_or_0c(struct emu_cpu *c, struct instruction *i);
+int32_t instr_or_0d(struct emu_cpu *c, struct instruction *i);
+
+
+/* adc */
+int32_t instr_adc_10(struct emu_cpu *c, struct instruction *i);
+int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i);
+int32_t instr_adc_12(struct emu_cpu *c, struct instruction *i);
+int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i);
+int32_t instr_adc_14(struct emu_cpu *c, struct instruction *i);
+int32_t instr_adc_15(struct emu_cpu *c, struct instruction *i);
+
+/* sbb */
+int32_t instr_sbb_18(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sbb_19(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sbb_1a(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sbb_1b(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sbb_1c(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sbb_1d(struct emu_cpu *c, struct instruction *i);
+
+
+/* and */
+int32_t instr_and_20(struct emu_cpu *c, struct instruction *i);
+int32_t instr_and_21(struct emu_cpu *c, struct instruction *i);
+int32_t instr_and_22(struct emu_cpu *c, struct instruction *i);
+int32_t instr_and_23(struct emu_cpu *c, struct instruction *i);
+int32_t instr_and_24(struct emu_cpu *c, struct instruction *i);
+int32_t instr_and_25(struct emu_cpu *c, struct instruction *i);
+
+/* sub */
+int32_t instr_sub_28(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sub_29(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sub_2a(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sub_2b(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sub_2c(struct emu_cpu *c, struct instruction *i);
+int32_t instr_sub_2d(struct emu_cpu *c, struct instruction *i);
+
+
+/* xor */
+int32_t instr_xor_30(struct emu_cpu *c, struct instruction *i);
+int32_t instr_xor_31(struct emu_cpu *c, struct instruction *i);
+int32_t instr_xor_32(struct emu_cpu *c, struct instruction *i);
+int32_t instr_xor_33(struct emu_cpu *c, struct instruction *i);
+int32_t instr_xor_34(struct emu_cpu *c, struct instruction *i);
+int32_t instr_xor_35(struct emu_cpu *c, struct instruction *i);
+
+/* cmp */
+int32_t instr_cmp_38(struct emu_cpu *c, struct instruction *i);
+int32_t instr_cmp_39(struct emu_cpu *c, struct instruction *i);
+int32_t instr_cmp_3a(struct emu_cpu *c, struct instruction *i);
+int32_t instr_cmp_3b(struct emu_cpu *c, struct instruction *i);
+int32_t instr_cmp_3c(struct emu_cpu *c, struct instruction *i);
+int32_t instr_cmp_3d(struct emu_cpu *c, struct instruction *i);
+
+
 #endif /*HAVE_EMU_CPU_FUNCTIONS_H*/
---
+++ src/functions/adc.c
@@ -0,0 +1,219 @@
+/* @header@ */
+#include <stdint.h>
+
+// FIXME replace the INSTR_CALC macro and verify the flags
+
+#include "emu/emu_cpu.h"
+#include "emu/emu_cpu_data.h"
+#include "emu/emu_cpu_functions.h"
+#include "emu/emu_memory.h"
+
+
+#ifdef INSTR_CALC_AND_SET_FLAGS
+#undef INSTR_CALC_AND_SET_FLAGS
+#endif // INSTR_CALC_AND_SET_FLAGS
+
+#define INSTR_CALC_AND_SET_FLAGS(inttype,cpu,a,b,c,operation)	\
+INSTR_CALC(inttype,a,b,c,operation)								\
+INSTR_SET_FLAG_ZF(cpu)											\
+INSTR_SET_FLAG_PF(cpu)											\
+INSTR_SET_FLAG_SF(cpu)											\
+INSTR_SET_FLAG_CF(cpu,operation)								\
+INSTR_SET_FLAG_OF(cpu,operation)								
+
+
+int32_t instr_adc_10(struct emu_cpu *c, struct instruction *i)
+{
+	if ( i->modrm.mod != 3 )
+	{
+		uint8_t dst;
+		MEM_BYTE_READ(c, i->modrm.ea, &dst);
+//		dst += *c->reg8[i->modrm.opc];
+		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
+								 c, 
+								 dst, 
+								 *c->reg8[i->modrm.opc], 
+								 dst, 
+								 +)
+		MEM_BYTE_WRITE(c, i->modrm.ea, dst);
+	} else
+	{
+/*		*c->reg8[i->modrm.rm] += *c->reg8[i->modrm.opc]; */
+		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
+								 c, 
+								 *c->reg8[i->modrm.rm], 
+								 *c->reg8[i->modrm.opc], 
+								 *c->reg8[i->modrm.rm], 
+								 +)
+	}
+
+	return 0;
+}
+
+int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
+{
+	if ( i->modrm.mod != 3 )
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+			uint16_t dst;
+			MEM_WORD_READ(c, i->modrm.ea, &dst);
+//			dst += *c->reg16[i->modrm.opc];
+			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
+									 c, 
+									 dst, 
+									 *c->reg16[i->modrm.opc], 
+									 dst, 
+									 +)
+			MEM_WORD_WRITE(c, i->modrm.ea, dst);
+		} else
+		{
+			uint32_t dst;
+			MEM_DWORD_READ(c, i->modrm.ea, &dst);
+//			dst += c->reg[i->modrm.opc];
+			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
+									 c, 
+									 dst, 
+									 c->reg[i->modrm.opc], 
+									 dst, 
+									 +)
+			MEM_DWORD_WRITE(c, i->modrm.ea, dst);
+		}
+	} else
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+//			*c->reg16[i->modrm.rm] += *c->reg16[i->modrm.opc];
+			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
+									 c, 
+									 *c->reg16[i->modrm.rm], 
+									 *c->reg16[i->modrm.opc], 
+									 *c->reg16[i->modrm.rm], 
+									 +)
+		} else
+		{
+//			c->reg[i->modrm.rm] += c->reg[i->modrm.opc];
+			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
+									 c, 
+									 c->reg[i->modrm.rm], 
+									 c->reg[i->modrm.opc], 
+									 c->reg[i->modrm.rm], 
+									 +)
+		}
+	}
+
+	return 0;
+}
+
+int32_t instr_adc_12(struct emu_cpu *c, struct instruction *i)
+{
+	if ( i->modrm.mod != 3 )
+	{
+		uint8_t op;
+		MEM_BYTE_READ(c, i->modrm.ea, &op);
+
+//		*c->reg8[i->modrm.opc] += op;
+		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
+								 c, 
+								 op, 
+								 *c->reg8[i->modrm.opc], 
+								 *c->reg8[i->modrm.opc], 
+								 +)
+	} else
+	{
+//		*c->reg8[i->modrm.opc] += *c->reg8[i->modrm.rm];
+		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
+								 c, 
+								 *c->reg8[i->modrm.opc], 
+								 *c->reg8[i->modrm.rm], 
+								 *c->reg8[i->modrm.opc], 
+								 +)
+	}
+
+	return 0;
+}
+
+int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
+{
+	if ( i->modrm.mod != 3 )
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+			uint16_t op;
+			MEM_WORD_READ(c, i->modrm.ea, &op);
+
+//			*c->reg16[i->modrm.opc] += op;
+			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
+									 c, 
+									 op,
+									 *c->reg16[i->modrm.opc], 
+									 *c->reg16[i->modrm.opc], 
+									 +)
+		} else
+		{
+			uint32_t op;
+			MEM_DWORD_READ(c, i->modrm.ea, &op);
+//			c->reg[i->modrm.opc] += op;
+			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
+									 c, 
+									 op,
+									 c->reg[i->modrm.opc], 
+									 c->reg[i->modrm.opc], 
+									 +)
+		}
+	} else
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+//			*c->reg16[i->modrm.opc] += *c->reg16[i->modrm.rm];
+			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
+									 c, 
+									 *c->reg16[i->modrm.rm], 
+									 *c->reg16[i->modrm.opc], 
+									 *c->reg16[i->modrm.opc], 
+									 +)
+		} else
+		{
+			c->reg[i->modrm.opc] += c->reg[i->modrm.rm];
+		}
+	}
+
+	return 0;
+}
+
+int32_t instr_adc_14(struct emu_cpu *c, struct instruction *i)
+{
+//	*c->reg8[eax] += *i->imm8;
+	INSTR_CALC_AND_SET_FLAGS(uint8_t, 
+							 c, 
+							 *c->reg8[eax], 
+							 *i->imm8, 
+							 *c->reg8[eax], 
+							 +)
+	return 0;
+}
+
+int32_t instr_adc_15(struct emu_cpu *c, struct instruction *i)
+{
+	if ( i->prefixes & PREFIX_OPSIZE )
+	{
+//		*c->reg16[eax] += *i->imm16;
+		INSTR_CALC_AND_SET_FLAGS(uint16_t, 
+								 c, 
+								 *c->reg16[eax], 
+								 *i->imm16, 
+								 *c->reg16[eax], 
+								 +)
+	} else
+	{
+//		c->reg[eax] += i->imm;
+		INSTR_CALC_AND_SET_FLAGS(uint32_t, 
+								 c, 
+								 c->reg[eax], 
+								 i->imm, 
+								 c->reg[eax], 
+								 +)
+	}
+
+	return 0;
+}
