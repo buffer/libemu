cbd4f5477218c4b8b61e7040404817d1f3047409
common <common@carnivore.it>
2007-02-13 16:16:04 +0000

    limemu  - comment adc, add the forgotten makro

--- src/functions/adc.c
+++ src/functions/adc.c
@@ -62,16 +62,20 @@ INSTR_SET_FLAG_PF(cpu)											\
 INSTR_SET_FLAG_SF(cpu)											\
 INSTR_SET_FLAG_OF(cpu,operation)								\
 INSTR_SET_FLAG_CF(cpu,operation)
-								
+
+
 
 
 int32_t instr_adc_10(struct emu_cpu *c, struct instruction *i)
 {
+	/* 10 /r
+	 * Add with carry byte register to r/m8
+	 * ADC r/m8,r8     
+	 */
 	if ( i->modrm.mod != 3 )
 	{
 		uint8_t dst;
 		MEM_BYTE_READ(c, i->modrm.ea, &dst);
-//		dst += *c->reg8[i->modrm.opc];
 		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
 								 c, 
 								 dst, 
@@ -81,7 +85,6 @@ int32_t instr_adc_10(struct emu_cpu *c, struct instruction *i)
 		MEM_BYTE_WRITE(c, i->modrm.ea, dst);
 	} else
 	{
-/*		*c->reg8[i->modrm.rm] += *c->reg8[i->modrm.opc]; */
 		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
 								 c, 
 								 *c->reg8[i->modrm.rm], 
@@ -95,13 +98,18 @@ int32_t instr_adc_10(struct emu_cpu *c, struct instruction *i)
 
 int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
 {
+
+
 	if ( i->modrm.mod != 3 )
 	{
 		if ( i->prefixes & PREFIX_OPSIZE )
 		{
+			/* 11 /r
+			 * Add with carry r16 to r/m16
+			 * ADC r/m16,r16   
+			 */
 			uint16_t dst;
 			MEM_WORD_READ(c, i->modrm.ea, &dst);
-//			dst += *c->reg16[i->modrm.opc];
 			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
 									 c, 
 									 dst, 
@@ -111,9 +119,12 @@ int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
 			MEM_WORD_WRITE(c, i->modrm.ea, dst);
 		} else
 		{
+			/* 11 /r
+			 * Add with CF r32 to r/m32
+			 * ADC r/m32,r32   
+			 */
 			uint32_t dst;
 			MEM_DWORD_READ(c, i->modrm.ea, &dst);
-//			dst += c->reg[i->modrm.opc];
 			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
 									 c, 
 									 dst, 
@@ -126,7 +137,10 @@ int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
 	{
 		if ( i->prefixes & PREFIX_OPSIZE )
 		{
-//			*c->reg16[i->modrm.rm] += *c->reg16[i->modrm.opc];
+			/* 11 /r
+			 * Add with carry r16 to r/m16
+			 * ADC r/m16,r16   
+			 */
 			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
 									 c, 
 									 *c->reg16[i->modrm.rm], 
@@ -135,7 +149,10 @@ int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
 									 +)
 		} else
 		{
-//			c->reg[i->modrm.rm] += c->reg[i->modrm.opc];
+			/* 11 /r
+			 * Add with CF r32 to r/m32
+			 * ADC r/m32,r32   
+			 */
 			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
 									 c, 
 									 c->reg[i->modrm.rm], 
@@ -150,12 +167,16 @@ int32_t instr_adc_11(struct emu_cpu *c, struct instruction *i)
 
 int32_t instr_adc_12(struct emu_cpu *c, struct instruction *i)
 {
+	/* 12 /r
+	 * Add with carry r/m8 to byte register
+	 * ADC r8,r/m8     
+	 */
+
 	if ( i->modrm.mod != 3 )
 	{
 		uint8_t op;
 		MEM_BYTE_READ(c, i->modrm.ea, &op);
 
-//		*c->reg8[i->modrm.opc] += op;
 		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
 								 c, 
 								 op, 
@@ -164,7 +185,6 @@ int32_t instr_adc_12(struct emu_cpu *c, struct instruction *i)
 								 +)
 	} else
 	{
-//		*c->reg8[i->modrm.opc] += *c->reg8[i->modrm.rm];
 		INSTR_CALC_AND_SET_FLAGS(uint8_t, 
 								 c, 
 								 *c->reg8[i->modrm.opc], 
@@ -178,14 +198,19 @@ int32_t instr_adc_12(struct emu_cpu *c, struct instruction *i)
 
 int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
 {
+
+
 	if ( i->modrm.mod != 3 )
 	{
 		if ( i->prefixes & PREFIX_OPSIZE )
 		{
+			/* 13 /r
+			 * Add with carry r/m16 to r16
+			 * ADC r16,r/m16   
+			 */
 			uint16_t op;
 			MEM_WORD_READ(c, i->modrm.ea, &op);
 
-//			*c->reg16[i->modrm.opc] += op;
 			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
 									 c, 
 									 op,
@@ -194,9 +219,12 @@ int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
 									 +)
 		} else
 		{
+			/* 13 /r
+			 * Add with CF r/m32 to r32
+			 * ADC r32,r/m32   
+			 */
 			uint32_t op;
 			MEM_DWORD_READ(c, i->modrm.ea, &op);
-//			c->reg[i->modrm.opc] += op;
 			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
 									 c, 
 									 op,
@@ -208,7 +236,11 @@ int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
 	{
 		if ( i->prefixes & PREFIX_OPSIZE )
 		{
-//			*c->reg16[i->modrm.opc] += *c->reg16[i->modrm.rm];
+			/* 13 /r
+			 * Add with carry r/m16 to r16
+			 * ADC r16,r/m16   
+			 */
+
 			INSTR_CALC_AND_SET_FLAGS(uint16_t, 
 									 c, 
 									 *c->reg16[i->modrm.rm], 
@@ -217,7 +249,16 @@ int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
 									 +)
 		} else
 		{
-			c->reg[i->modrm.opc] += c->reg[i->modrm.rm];
+			/* 13 /r
+			 * Add with CF r/m32 to r32
+			 * ADC r32,r/m32   
+			 */
+			INSTR_CALC_AND_SET_FLAGS(uint32_t, 
+									 c, 
+									 c->reg[i->modrm.rm], 
+									 c->reg[i->modrm.opc], 
+									 c->reg[i->modrm.opc], 
+									 +)
 		}
 	}
 
@@ -226,30 +267,40 @@ int32_t instr_adc_13(struct emu_cpu *c, struct instruction *i)
 
 int32_t instr_adc_14(struct emu_cpu *c, struct instruction *i)
 {
-//	*c->reg8[eax] += *i->imm8;
+	/* 14 ib 
+	 * Add with carry imm8 to AL
+	 * ADC AL,imm8
+	 */
 	INSTR_CALC_AND_SET_FLAGS(uint8_t, 
 							 c, 
-							 *c->reg8[eax], 
+							 *c->reg8[al], 
 							 *i->imm8, 
-							 *c->reg8[eax], 
+							 *c->reg8[al], 
 							 +)
 	return 0;
 }
 
 int32_t instr_adc_15(struct emu_cpu *c, struct instruction *i)
 {
+
 	if ( i->prefixes & PREFIX_OPSIZE )
 	{
-//		*c->reg16[eax] += *i->imm16;
+		/* 15 iw
+		 * Add with carry imm16 to AX
+		 * ADC AX,imm16
+		 */
 		INSTR_CALC_AND_SET_FLAGS(uint16_t, 
 								 c, 
-								 *c->reg16[eax], 
+								 *c->reg16[ax], 
 								 *i->imm16, 
-								 *c->reg16[eax], 
+								 *c->reg16[ax], 
 								 +)
 	} else
 	{
-//		c->reg[eax] += i->imm;
+		/* 15 id 
+		 * Add with carry imm32 to EAX
+		 * ADC EAX,imm32
+		 */
 		INSTR_CALC_AND_SET_FLAGS(uint32_t, 
 								 c, 
 								 c->reg[eax], 
