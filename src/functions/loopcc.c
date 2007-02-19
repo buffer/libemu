f14936b328a19cabfc88545623b2c9e978770a89
common <common@carnivore.it>
2007-02-19 16:42:56 +0000

    libemu  - implement loop/loopcc

--- src/functions/loopcc.c
+++ src/functions/loopcc.c
@@ -15,7 +15,21 @@ int32_t instr_loopcc_e0(struct emu_cpu *c, struct instruction *i)
 	 * LOOPNE rel8
 	 * LOOPNZ rel8 
 	 */
-
+	if ( i->prefixes & PREFIX_OPSIZE)
+	{
+		*c->reg16[cx] = *c->reg16[cx]-1;
+		if (*c->reg16[cx] != 0 && !CPU_FLAG_ISSET(c,f_zf))
+		{
+			c->eip += *i->imm8;
+		}
+	}else
+	{
+		c->reg[ecx]--;
+		if (c->reg[ecx] != 0 && !CPU_FLAG_ISSET(c,f_zf))
+		{
+			c->eip += *i->imm8;
+		}
+	}
 
 
 	return 0;
@@ -30,6 +44,22 @@ int32_t instr_loopcc_e1(struct emu_cpu *c, struct instruction *i)
 	 * LOOPE rel8  
 	 * LOOPZ rel8  
 	 */				   
+	if ( i->prefixes & PREFIX_OPSIZE)
+	{
+		*c->reg16[cx] = *c->reg16[cx]-1;
+		if (*c->reg16[cx] != 0 && CPU_FLAG_ISSET(c,f_zf))
+		{
+			c->eip += *i->imm8;
+		}
+	}else
+	{
+		c->reg[ecx]--;
+		if (c->reg[ecx] != 0 && CPU_FLAG_ISSET(c,f_zf))
+		{
+			c->eip += *i->imm8;
+		}
+	}
+
 
 	return 0;
 }
@@ -41,6 +71,21 @@ int32_t instr_loop_e2(struct emu_cpu *c, struct instruction *i)
 	 * LOOP rel8   
 	 */
 
+	if ( i->prefixes & PREFIX_OPSIZE)
+	{
+		*c->reg16[cx] = *c->reg16[cx]-1;
+		if (*c->reg16[cx] != 0 )
+		{
+			c->eip += *i->imm8;
+		}
+	}else
+	{
+		c->reg[ecx]--;
+		if (c->reg[ecx] != 0)
+		{
+			c->eip += *i->imm8;
+		}
+	}
 
 
 	return 0;
