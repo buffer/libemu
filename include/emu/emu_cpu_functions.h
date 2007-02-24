c3082334845fb5702b73c641538780caea214abb
common <common@carnivore.it>
2007-02-24 17:09:41 +0000

    libemu  - use signed ints for memory axs

--- include/emu/emu_cpu_functions.h
+++ include/emu/emu_cpu_functions.h
@@ -3,32 +3,32 @@
 #define HAVE_EMU_CPU_FUNCTIONS_H
 
 #define MEM_BYTE_READ(cpu_p, addr, data_p) \
- { uint32_t ret = emu_memory_read_byte((cpu_p)->mem, addr, data_p); \
+ { int32_t ret = emu_memory_read_byte((cpu_p)->mem, addr, data_p); \
  if( ret != 0 ) \
   return ret; }
 
 #define MEM_BYTE_WRITE(cpu_p, addr, data) \
- { uint32_t ret = emu_memory_write_byte((cpu_p)->mem, addr, data); \
+ { int32_t ret = emu_memory_write_byte((cpu_p)->mem, addr, data); \
  if( ret != 0 ) \
   return ret; }
 
 #define MEM_WORD_READ(cpu_p, addr, data_p) \
- { uint32_t ret = emu_memory_read_word((cpu_p)->mem, addr, data_p); \
+ { int32_t ret = emu_memory_read_word((cpu_p)->mem, addr, data_p); \
  if( ret != 0 ) \
   return ret; }
 
 #define MEM_WORD_WRITE(cpu_p, addr, data) \
- { uint32_t ret = emu_memory_write_word((cpu_p)->mem, addr, data); \
+ { int32_t ret = emu_memory_write_word((cpu_p)->mem, addr, data); \
  if( ret != 0 ) \
   return ret; }
 
 #define MEM_DWORD_READ(cpu_p, addr, data_p) \
- { uint32_t ret = emu_memory_read_dword((cpu_p)->mem, addr, data_p); \
+ { int32_t ret = emu_memory_read_dword((cpu_p)->mem, addr, data_p); \
  if( ret != 0 ) \
   return ret; }
 
 #define MEM_DWORD_WRITE(cpu_p, addr, data) \
- { uint32_t ret = emu_memory_write_dword((cpu_p)->mem, addr, data); \
+ { int32_t ret = emu_memory_write_dword((cpu_p)->mem, addr, data); \
  if( ret != 0 ) \
   return ret; }
 
