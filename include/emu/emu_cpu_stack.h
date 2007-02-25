33612eab683d02c4f3826b5dca28fe3cbbe42b20
common <common@carnivore.it>
2007-02-25 05:20:34 +0000

    libemu  - if we push esp, we have to push the esp value before decrementing esp, therefore save a copy of arg in the stack macros and use the copy when writing to mem

--- include/emu/emu_cpu_stack.h
+++ include/emu/emu_cpu_stack.h
@@ -2,52 +2,61 @@
 #ifndef EMU_CPU_STACK_H_
 #define EMU_CPU_STACK_H_
 
-#define PUSH_DWORD(cpu, arg)				\
-if (cpu->reg[esp] < 4)								\
-{													\
-	emu_errno_set((cpu)->emu, ENOMEM);				\
-	emu_strerror_set((cpu)->emu,					\
-	"ran out of stack space writing a dword\n");	\
-	return -1;										\
-}													\
-cpu->reg[esp]-=4;									\
-{																				\
-		int32_t memret = emu_memory_write_dword(cpu->mem, cpu->reg[esp], arg);	\
+#define PUSH_DWORD(cpu, arg)							\
+{														\
+	uint32_t pushme = arg;								\
+	if (cpu->reg[esp] < 4)								\
+	{													\
+		emu_errno_set((cpu)->emu, ENOMEM);				\
+		emu_strerror_set((cpu)->emu,					\
+		"ran out of stack space writing a dword\n");	\
+		return -1;										\
+	}													\
+	cpu->reg[esp]-=4;									\
+	{																			\
+		int32_t memret = emu_memory_write_dword(cpu->mem, cpu->reg[esp], pushme);	\
 		if (memret != 0)														\
 			return memret;														\
+	}																			\
 }
 
 
-#define PUSH_WORD(cpu, arg)				\
-if (cpu->reg[esp] < 2)								\
-{													\
-	emu_errno_set((cpu)->emu, ENOMEM);				\
-	emu_strerror_set((cpu)->emu,					\
-	"ran out of stack space writing a word\n");		\
-	return -1;										\
-}													\
-cpu->reg[esp]-=2;									\
-{																				\
-		int32_t memret = emu_memory_write_word(cpu->mem, cpu->reg[esp], arg);	\
+#define PUSH_WORD(cpu, arg)								\
+{														\
+	uint16_t pushme  = arg;								\
+	if (cpu->reg[esp] < 2)								\
+	{													\
+		emu_errno_set((cpu)->emu, ENOMEM);				\
+		emu_strerror_set((cpu)->emu,					\
+		"ran out of stack space writing a word\n");		\
+		return -1;										\
+	}													\
+	cpu->reg[esp]-=2;									\
+	{																			\
+		int32_t memret = emu_memory_write_word(cpu->mem, cpu->reg[esp], pushme);\
 		if (memret != 0)														\
 			return memret;														\
+	}																			\
 }
 
 
 
-#define PUSH_BYTE(cpu, arg)				\
-if (cpu->reg[esp] < 1)								\
-{													\
-	emu_errno_set((cpu)->emu, ENOMEM);				\
-	emu_strerror_set((cpu)->emu,					\
-	"ran out of stack space writing a byte\n");		\
-	return -1;										\
-}													\
-cpu->reg[esp]-=1;									\
-{																				\
-		int32_t memret = emu_memory_write_byte(cpu->mem, cpu->reg[esp], arg);	\
-		if (memret != 0)														\
-			return memret;														\
+#define PUSH_BYTE(cpu, arg)								\
+{														\
+	uint8_t pushme = arg;								\
+	if (cpu->reg[esp] < 1)								\
+	{													\
+		emu_errno_set((cpu)->emu, ENOMEM);				\
+		emu_strerror_set((cpu)->emu,					\
+		"ran out of stack space writing a byte\n");		\
+		return -1;										\
+	}													\
+	cpu->reg[esp]-=1;									\
+	{																				\
+		int32_t memret = emu_memory_write_byte(cpu->mem, cpu->reg[esp], pushme);	\
+		if (memret != 0)															\
+			return memret;															\
+	}																				\
 }
 
 
