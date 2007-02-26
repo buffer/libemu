7b2bf1b84e8c6f83e43d28efccee4d478ac7ae3d
common <common@carnivore.it>
2007-02-26 22:50:50 +0000

    libemu  - add repcc.c as a reminder, includes pagenumber as a little gift for the one

---
+++ src/functions/repcc.c
@@ -0,0 +1,12 @@
+/* @header@ */
+#include <stdint.h>
+#include <errno.h>
+
+#include "emu/emu.h"
+#include "emu/emu_cpu.h"
+#include "emu/emu_cpu_data.h"
+#include "emu/emu_cpu_functions.h"
+#include "emu/emu_cpu_stack.h"
+#include "emu/emu_memory.h"
+
+/*Intel Architecture Software Developer's Manual Volume 2: Instruction Set Reference (24319102.PDF) page 645*/
