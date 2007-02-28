7336800df0516afe285fff76aab014f1193dd3c3
common <common@carnivore.it>
2007-02-28 02:34:48 +0000

    libemu  - implement scas cmps, use emu_memory_segment_{get,select} to save the current segment, switch to the required segment, and restore the old segment

--- src/functions/cmps.c
+++ src/functions/cmps.c
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <errno.h>
 
+#define INSTR_CALC(bits, a, b) \
+UINT(bits) operation_result = (a) - (b); \
+UINT(bits) operand_a = a; \
+UINT(bits) operand_b = b;
+
+
 #include "emu/emu.h"
 #include "emu/emu_cpu.h"
 #include "emu/emu_cpu_data.h"
@@ -11,33 +17,163 @@
 
 /*Intel Architecture Software Developer's Manual Volume 2: Instruction Set Reference (24319102.PDF) page 127*/
 
+
+#define INSTR_CALC_AND_SET_FLAGS(bits, cpu, a, b) \
+INSTR_CALC(bits, a, b) \
+INSTR_SET_FLAG_ZF(cpu) \
+INSTR_SET_FLAG_PF(cpu) \
+INSTR_SET_FLAG_SF(cpu) \
+INSTR_SET_FLAG_CF(cpu, -) \
+INSTR_SET_FLAG_OF(cpu, -,bits) 
+
+
+#define INSTR_CALC_EDI_ESI(cpu,bits) \
+{ \
+	if ( !CPU_FLAG_ISSET(cpu,f_df) ) \
+	{ \
+		cpu->reg[edi]+=bits/8; \
+		cpu->reg[esi]+=bits/8; \
+	} \
+	else \
+	{ \
+		cpu->reg[edi]-=bits/8; \
+		cpu->reg[esi]-=bits/8; \
+	} \
+}
+
+
+
 int32_t instr_cmps_a6(struct emu_cpu *c, struct instruction *i)
 {
+	if ( i->prefixes & PREFIX_ADSIZE )
+	{
+		/* A6 
+		 * Compares byte at address DS:SI with byte at address ES:DI and sets the status flags accordingly
+		 * CMPSB         
+		 * Compares byte at address DS:SI with byte at address ES:DI and sets the status flags accordingly
+		 * CMPS m8, m8   
+		 */
+	}
+	else
+	{
+		/* A6 
+		 * Compares byte at address DS:(E)SI with byte at address ES:(E)DI and sets the status flags accordingly
+		 * CMPSB         
+		 * Compares byte at address DS:(E)SI with byte at address ES:(E)DI and sets the status flags accordingly
+		 * CMPS m8, m8   
+		 */
 
-	/* A6 
-	 * Compares byte at address DS:(E)SI with byte at address ES:(E)DI and sets the status flags accordingly
-	 * CMPSB         
-	 * Compares byte at address DS:(E)SI with byte at address ES:(E)DI and sets the status flags accordingly
-	 * CMPS m8, m8   
-	 */
+		enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+
+		emu_memory_segment_select(c->mem,s_ds);
+		uint8_t m8a;
+		MEM_BYTE_READ(c, c->reg[esi], &m8a);
+
+		emu_memory_segment_select(c->mem,s_es);
+		uint8_t m8b;
+		MEM_BYTE_READ(c, c->reg[edi], &m8b);
+
+		emu_memory_segment_select(c->mem,oldseg);
+
+		INSTR_CALC_AND_SET_FLAGS(8,
+								 c,
+								 m8a,
+								 m8b)
+
+		INSTR_CALC_EDI_ESI(c, 8)
+	}
 	return 0;
 }
 
 
 int32_t instr_cmps_a7(struct emu_cpu *c, struct instruction *i)
 {
-	/* A7 
-	 * Compares word at address DS:(E)SI with word at address ES:(E)DI and sets the status flags accordingly
-	 * CMPS m16, m16 
-	 * Compares word at address DS:(E)SI with word at address ES:(E)DI and sets the status flags accordingly
-	 * CMPSW         
-	 */
+	if ( i->prefixes & PREFIX_ADSIZE )
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
 
-	/* A7 
-	 * Compares doubleword at address DS:(E)SI with doubleword at address ES:(E)DI and sets the status flags accordingly
-	 * CMPS m32, m32 
-	 * CMPSD         
-	 * Compares doubleword at address DS:(E)SI with doubleword at address ES:(E)DI and sets the status flags accordingly
-	 */
+			/* A7 
+			 * Compares word at address DS:SI with word at address ES:DI and sets the status flags accordingly
+			 * CMPS m16, m16 
+			 * Compares word at address DS:SI with word at address ES:DI and sets the status flags accordingly
+			 * CMPSW         
+			 */
+		}
+		else
+		{
+
+			/* A7 
+			 * Compares doubleword at address DS:SI with doubleword at address ES:DI and sets the status flags accordingly
+			 * CMPS m32, m32 
+			 * CMPSD         
+			 * Compares doubleword at address DS:SI with doubleword at address ES:DI and sets the status flags accordingly
+			 */
+		}
+	}
+	else
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+
+			/* A7 
+			 * Compares word at address DS:(E)SI with word at address ES:(E)DI and sets the status flags accordingly
+			 * CMPS m16, m16 
+			 * Compares word at address DS:(E)SI with word at address ES:(E)DI and sets the status flags accordingly
+			 * CMPSW         
+			 */
+
+			enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+
+			emu_memory_segment_select(c->mem,s_ds);
+			uint16_t m16a;
+			MEM_WORD_READ(c, c->reg[esi], &m16a);
+
+			emu_memory_segment_select(c->mem,s_es);
+			uint16_t m16b;
+			MEM_WORD_READ(c, c->reg[edi], &m16b);
+
+			emu_memory_segment_select(c->mem,oldseg);
+
+			INSTR_CALC_AND_SET_FLAGS(16,
+									 c,
+									 m16a,
+									 m16b)
+
+			INSTR_CALC_EDI_ESI(c, 16)
+
+		}
+		else
+		{
+
+			/* A7 
+			 * Compares doubleword at address DS:(E)SI with doubleword at address ES:(E)DI and sets the status flags accordingly
+			 * CMPS m32, m32 
+			 * CMPSD         
+			 * Compares doubleword at address DS:(E)SI with doubleword at address ES:(E)DI and sets the status flags accordingly
+			 */
+
+			enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+
+			emu_memory_segment_select(c->mem,s_ds);
+			uint32_t m32a;
+			MEM_DWORD_READ(c, c->reg[esi], &m32a);
+
+			emu_memory_segment_select(c->mem,s_es);
+			uint32_t m32b;
+			MEM_DWORD_READ(c, c->reg[edi], &m32b);
+
+			emu_memory_segment_select(c->mem,oldseg);
+
+			INSTR_CALC_AND_SET_FLAGS(32,
+									 c,
+									 m32a,
+									 m32b)
+
+			INSTR_CALC_EDI_ESI(c, 32)
+
+		}
+
+	}
 	return 0;
 }
--- src/functions/scas.c
+++ src/functions/scas.c
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <errno.h>
 
+#define INSTR_CALC(bits, a, b) \
+UINT(bits) operation_result = (a) - (b); \
+UINT(bits) operand_a = a; \
+UINT(bits) operand_b = b;
+
+
 #include "emu/emu.h"
 #include "emu/emu_cpu.h"
 #include "emu/emu_cpu_data.h"
@@ -11,33 +17,137 @@
 
 /*Intel Architecture Software Developer's Manual Volume 2: Instruction Set Reference (24319102.PDF) page 669*/
 
+#define INSTR_CALC_AND_SET_FLAGS(bits, cpu, a, b) \
+INSTR_CALC(bits, a, b) \
+INSTR_SET_FLAG_ZF(cpu) \
+INSTR_SET_FLAG_PF(cpu) \
+INSTR_SET_FLAG_SF(cpu) \
+INSTR_SET_FLAG_CF(cpu, -) \
+INSTR_SET_FLAG_OF(cpu, -,bits) 
+
+
+#define INSTR_CALC_EDI(cpu, bits) \
+{ \
+	if ( !CPU_FLAG_ISSET(cpu,f_df) ) \
+	cpu->reg[edi]+=bits/8; \
+else \
+	cpu->reg[edi]-=bits/8; \
+}
+
+
 int32_t instr_scas_ae(struct emu_cpu *c, struct instruction *i)
 {
-	/* AE 
-     * Compare AL with byte at ES:(E)DI and set status flags
-	 * SCAS m8  
-	 * Compare AL with byte at ES:(E)DI and set status flags
-	 * SCASB    
-	 */
+	if ( i->prefixes & PREFIX_ADSIZE )
+	{
+		/* AE 
+		 * Compare AL with byte at ES:DI and set status flags
+		 * SCAS m8  
+		 * Compare AL with byte at ES:DI and set status flags
+		 * SCASB    
+		 */
+
+	}
+	else
+	{
+		/* AE 
+		 * Compare AL with byte at ES:EDI and set status flags
+		 * SCAS m8  
+		 * Compare AL with byte at ES:EDI and set status flags
+		 * SCASB    
+		 */
+		enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+		emu_memory_segment_select(c->mem,s_es);
+
+		uint8_t m8;
+		MEM_BYTE_READ(c, c->reg[edi], &m8);
+
+		emu_memory_segment_select(c->mem,oldseg);
+
+		INSTR_CALC_AND_SET_FLAGS(8,
+								 c,
+								 *c->reg8[al],
+								 m8)
+		INSTR_CALC_EDI(c, 8)
+		
+	}
 	return 0;
 }
 
 
 int32_t instr_scas_af(struct emu_cpu *c, struct instruction *i)
 {
-	/* AF 
-	 * Compare AX with word at ES:(E)DI and set status flags
-	 * SCAS m16 
-	 * Compare AX with word at ES:(E)DI and set status flags
-	 * SCASW    
-	 */
+	if ( i->prefixes & PREFIX_ADSIZE )
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+			/* AF 
+			 * Compare AX with word at ES:DI and set status flags
+			 * SCAS m16 
+			 * Compare AX with word at ES:DI and set status flags
+			 * SCASW    
+			 */
+		}
+		else
+		{
+			/* AF 
+			 * Compare EAX with doubleword at ES:DI and set status flags
+			 * SCAS m32 
+			 * Compare EAX with doubleword at ES:DI and set status flags
+			 * SCASD    
+			 */
 
-	/* AF 
-	 * Compare EAX with doubleword at ES(E)DI and set status flags
-	 * SCAS m32 
-	 * Compare EAX with doubleword at ES:(E)DI and set status flags
-	 * SCASD    
-	 */
+		}
 
+	}else
+	{
+		if ( i->prefixes & PREFIX_OPSIZE )
+		{
+			/* AF 
+			 * Compare AX with word at ES:EDI and set status flags
+			 * SCAS m16 
+			 * Compare AX with word at ES:EDI and set status flags
+			 * SCASW    
+			 */
+
+			enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+			emu_memory_segment_select(c->mem,s_es);
+
+			uint16_t m16;
+			MEM_WORD_READ(c, c->reg[edi], &m16);
+
+			emu_memory_segment_select(c->mem,oldseg);
+
+			INSTR_CALC_AND_SET_FLAGS(8,
+									 c,
+									 *c->reg16[ax],
+									 m16)
+
+			INSTR_CALC_EDI(c, 16)
+		}
+		else
+		{
+			/* AF 
+			 * Compare EAX with doubleword at ES:EDI and set status flags
+			 * SCAS m32 
+			 * Compare EAX with doubleword at ES:EDI and set status flags
+			 * SCASD    
+			 */
+
+			enum emu_segment oldseg = emu_memory_segment_get(c->mem);
+			emu_memory_segment_select(c->mem,s_es);
+
+			uint32_t m32;
+			MEM_DWORD_READ(c, c->reg[edi], &m32);
+
+			emu_memory_segment_select(c->mem,oldseg);
+
+			INSTR_CALC_AND_SET_FLAGS(32,
+									 c,
+									 c->reg[eax],
+									 m32)
+
+			INSTR_CALC_EDI(c, 32)
+		}
+	}
 	return 0;
 }
