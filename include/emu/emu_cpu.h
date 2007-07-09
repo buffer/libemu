d923da73c7df6c9e6ad4abdf9aa2c14b2be59d2c
common <common@carnivore.it>
2007-07-09 15:16:10 +0000

    libemu  - gplv2 boilerplate for all files but libdasm

--- include/emu/emu_cpu.h
+++ include/emu/emu_cpu.h
@@ -1,4 +1,59 @@
-/* @header@ */
+/********************************************************************************
+ *                               libemu
+ *
+ *                           - call 0x38f - 
+ *                       - Emus are large birds -
+ *                - a part of the the finest collection -
+ *
+ *
+ * Copyright (C) 2007  Paul Baecher & Markus Koetter
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * 
+ *             contact nepenthesdev@users.sourceforge.net  
+ *
+ *******************************************************************************/
+
+/********************************************************************************
+ *                               libemu
+ *
+ *                    - x86 shellcode emulation -
+ *
+ *
+ * Copyright (C) 2007  Paul Baecher & Markus Koetter
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * 
+ *             contact nepenthesdev@users.sourceforge.net  
+ *
+ *******************************************************************************/
+
 #ifndef HAVE_EMU_CPU_H
 #define HAVE_EMU_CPU_H
 
