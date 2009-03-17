ed5d46738de1a5ca989240c258357d91104ac34c
common <common@carnivore.it>
2009-03-17 11:10:43 +0000

    libemu  - respect more include hierarchies for inet_

--- src/environment/linux/env_linux_syscall_hooks.c
+++ src/environment/linux/env_linux_syscall_hooks.c
@@ -26,11 +26,11 @@
  *******************************************************************************/
 
 // for the socket hooks
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+
 #include <stdlib.h>
 
 
